20db5e66b8fcf79c1027d65021327290b6bf28ce
Yex <officialsigmemer@gmail.com>
2023-06-23 18:45:14 -0400

    chat changes & fixes

--- chat.js
+++ chat.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyAa9nIvVqumqkClFO8il19Va0KoQ_wmN8M",
   authDomain: "yexs-chat.firebaseapp.com",
   databaseURL: "https://yexs-chat-default-rtdb.firebaseio.com",
@@ -6,15 +6,14 @@ const firebaseConfig = {
   storageBucket: "yexs-chat.appspot.com",
   messagingSenderId: "784190773413",
   appId: "1:784190773413:web:5de305c6e34bc779a6154c"
-};
-
-firebase.initializeApp(firebaseConfig);
+});
 const database = firebase.database();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
 const elem = (query) => document.querySelector(query);
 const elems = (query) => Array.from(document.querySelectorAll(query));
+const channelContent = elem("ul#channel-content");
 
 elem("#message-form").addEventListener("submit", messageSub);
 elem("#message-input").addEventListener("keydown", function(ev) {
@@ -63,23 +62,25 @@ function createChannel(name, type, categoryName) {
   `));
 }
 
-function createChatMessage(sender, content, timestamp) {
+function createChatMessage(content, sender={}, timestamp=Date.now()) {
   if(content.trim() !== '') {
-    document.querySelector("ul#channel-content").insertAdjacentHTML("beforeend", `
-      <li class="chat-message" data-status="${(sender.uid || "") === auth.currentUser.uid ? 'sent' : 'received'}" data-timestamp="${(timestamp || Date.now())}"> 
-        <img class='profile-pic' src="${sender.photoURL}" alt="Profile Pic" loading="lazy">
+    const prevMax = channelContent.scrollHeight - channelContent.clientHeight;
+    channelContent.insertAdjacentHTML("beforeend", `
+      <li class="chat-message" data-status="${(sender.uid || '') === auth.currentUser.uid ? 'sent' : 'received'}" data-timestamp="${(timestamp || Date.now())}"> 
+        <img class='profile-pic' src="${(sender.photoURL || "./favicon.png")}" alt="Profile Pic" loading="lazy">
         <div class="user-message">
-          <p class="username">${(sender.displayName || "User")} <time class="timestamp" datetime="${new Date(parseInt((timestamp || Date.now()))).toISOString()}">${new Date(parseInt((timestamp || Date.now()))).toLocaleString('en-US', { dateStyle: "short", timeStyle: "short" })}</time></p>
+          <p class="username">${(sender.displayName || "Chat Bot <span class='bot'>BOT</span>")} <time class="timestamp" datetime="${new Date(timestamp).toISOString()}">${new Date(timestamp).toLocaleString('en-US', { dateStyle: "short", timeStyle: "short" })}</time></p>
           <div class="message-content">${replaceArray(content.trim(), ["\&", "\<", "\>", /*/`{3,4} *(\w*)\s+((?:.|\s)*)`{3,4}/g,*/ /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)/g, /@\S+/g], ["&amp;", "&lt;", "&gt;", /*(_, lang, code) => "<pre><code>" + hljs.highlight(code, { language: lang || "plaintext" }).value + "</code></pre>",*/ full => `<a href="${full}">${full}</a>`, full => `<span class='mcolor'>${full}</span>`])}</div>
         </div>
       </li>
     `);
-    scrollChatMessages();
+    if(channelContent.scrollTop === prevMax || (sender.uid || '') === auth.currentUser.uid)
+      scrollChatMessages();
   }
 }
 
 function extractKeys(keys, from) {
-  return Object.fromEntries(Object.entries(from).filter(([key]) => keys.includes(key)));
+  return Object.fromEntries(Object.entries(from || {}).filter(([key]) => keys.includes(key)));
 }
 
 function errorNotif(msg) {
@@ -93,38 +94,41 @@ function errorNotif(msg) {
   });
 }
 
-function sendMessage(user, message, messageInput) {
+function sendMessage(message, clearMessageInput=true) {
   if(message.trim() !== '' && !message.toLowerCase().includes(atob("bmlnZ2Vy"))) {
     database.ref(`channels/${location.hash.slice(1)}/messages/${Date.now()}`).set({
-      user,
+      user: extractKeys(["displayName", "photoURL", "uid"], auth.currentUser),
       message
-    }).then(() => { if(messageInput) messageInput.value = '' }).catch(err => errorNotif(err.message));
+    }).then(() => clearMessageInput ? elem("#message-input").value = '' : null).catch(err => errorNotif(err.message));
   } else errorNotif("Enter A Valid Message!");
 }
+
+const commands = {
+  "shrug": function() {
+    sendMessage([...arguments].join(' ') + " ¯\\_(ツ)_/¯");
+  },
+  "tableflip": function() {
+    sendMessage([...arguments].join(' ') + " (╯°□°）╯︵ ┻━┻");
+  },
+  "help": function() {
+    createChatMessage("Commands:\n" + Object.keys(this).map(v => `\t>\t/${v}`).join('\n'));
+  }
+};
 
 function messageSub(e) {
     e.preventDefault();
 
     const messageInput = elem("#message-input");
     const message = messageInput.value; 
-    const user = extractKeys(["displayName", "photoURL", "uid"], auth.currentUser);
 
     if(message.startsWith('/')) {
-      const cmdName = message.slice(1).split(' ').shift();
+      const cmdName = message.slice(1).split(' ').shift().toLowerCase();
       const cmdArgs = message.split(' ').slice(1);
 
-      switch(cmdName) {
-        case "tableflip":
-          sendMessage(user, "(╯°□°）╯︵ ┻━┻", messageInput);
-          break;
-        case "shrug":
-          sendMessage(user, "¯\\_(ツ)_/¯", messageInput);
-          break;
-        default:
-          errorNotif("Unknown Command.");
-          break;
-      }
-    } else sendMessage(user, message, messageInput);
+      if(Object.keys(commands).includes(cmdName)) 
+        commands[cmdName](...cmdArgs);
+      else errorNotif("Unknown Command.");
+    } else sendMessage(message);
 }
 
 elem('i[class^="fa"]#eye').onclick = function() {
@@ -145,19 +149,16 @@ if("user-email" in localStorage && "user-password" in localStorage) {
     elem("#user-form").toggleAttribute("hidden");
     elem("#chat").toggleAttribute("hidden");
     scrollChatMessages();
-  }).catch(thrownError => { 
-    errorNotif(thrownError.message);
-    console.error(thrownError);
-  });
+  }).catch(thrownError => errorNotif(thrownError.message));
 }
 
 elem("#form-change").onclick = function() {
   if(elem("#user-form").getAttribute("data-mode") === "login") {
     this.textContent = "Login";
-    elem("#user-form h1").textContent = "Sign Up";
+    elem("#user-form h1").textContent = "Register";
     elem("#user-form").setAttribute("data-mode", "register");
   } else if(elem("#user-form").getAttribute("data-mode") === "register") {
-    this.textContent = "Sign Up";
+    this.textContent = "Register";
     elem("#user-form h1").textContent = "Login";
     elem("#user-form").setAttribute("data-mode", "login");
   }
@@ -167,22 +168,19 @@ elem("#form-change").onclick = function() {
 elem("#user-form").onsubmit = function(ev) { 
    ev.preventDefault();
 
-   if(this.getAttribute("data-mode").toLowerCase() === "login")
+   if(this.getAttribute("data-mode") === "login")
       auth.signInWithEmailAndPassword(elem("#email-input").value, elem("#password-input").value).then(async userCredential => {
         loadMessages();
         const user = userCredential.user;
         const photoRef = storage.ref(`userPhotos/${user.uid}`);
         const photoURL = await photoRef.getDownloadURL();
-        elem("#user > img.profile-pic").setAttribute("src", photoURL);
-        elem("#user > #user-info > p.username").textContent = user.displayName;
+        elem("#user img.profile-pic").setAttribute("src", photoURL);
+        elem("#user p.username").textContent = user.displayName;
         this.toggleAttribute("hidden");
         elem("#chat").toggleAttribute("hidden");
         scrollChatMessages();
-      }).catch(thrownError => { 
-        errorNotif(thrownError.message);
-        console.error(thrownError);
-      });
-   else if(this.getAttribute("data-mode").toLowerCase() === "register")
+      }).catch(thrownError => errorNotif(thrownError.message));
+   else if(this.getAttribute("data-mode") === "register")
       auth.createUserWithEmailAndPassword(elem("#email-input").value, elem("#password-input").value).then(async userCredential => {
         loadMessages();
         const user = userCredential.user;
@@ -190,18 +188,15 @@ elem("#user-form").onsubmit = function(ev) {
         const photoFile = elem("#photo-input").files[0];
         if(displayName.length > 25 || displayName.toLowerCase().includes(atob('bmlnZ2Vy')))
           throw new TypeError((displayName.length > 25 ? "Username is too long! " : '') + (displayName.toLowerCase().includes(atob("bmlnZ2Vy")) ? "That username is already in use!" : ''));
-        const photoRef = (await Promise.resolve(storage.ref("userPhotos").child(user.uid).put(photoFile))).ref;
+        const photoRef = (await storage.ref("userPhotos").child(user.uid).put(photoFile)).ref;
         const photoURL = await photoRef.getDownloadURL();
         user.updateProfile({ displayName, photoURL });
-        elem("#user > img.profile-pic").setAttribute("src", photoURL);
-        elem("#user > #user-info > p.username").textContent = displayName;
+        elem("#user img.profile-pic").setAttribute("src", photoURL);
+        elem("#user p.username").textContent = displayName;
         this.toggleAttribute("hidden");
         elem("#chat").toggleAttribute("hidden");
         scrollChatMessages();
-      }).catch(thrownError => { 
-        errorNotif(thrownError.message);
-        console.error(thrownError);
-      });
+      }).catch(thrownError => errorNotif(thrownError.message));
       if(this.querySelector("button.toggle-btn").classList.contains("active")) {
         localStorage.setItem("user-email", elem("#email-input").value);
         localStorage.setItem("user-password", elem("#password-input").value);
@@ -224,31 +219,27 @@ function openFilePicker({ accept="*", multiple=false }) {
 elem("div#user img.profile-pic").onclick = async function() {
   const file = await openFilePicker({ accept: "image/png, image/jpeg" });
   const photoRef = storage.ref("userPhotos/" + auth.currentUser.uid);
-  photoRef.put(file);
-  const photoURL = await photoRef.getDownloadURL();
-  auth.currentUser.updateProfile({ photoURL });
-  this.src = photoURL;
+  await photoRef.put(file);
+  this.src = this.src + '?' + Date.now();
 }
 
 function chatChannelChildAdded(snapshot) {
-  createChatMessage(snapshot.val().user, snapshot.val().message, snapshot.key);
+  createChatMessage(snapshot.val().message, snapshot.val().user, Number(snapshot.key));
 }
   
 function chatChannelChildRemoved(snapshot) {
-  for(const listItem of document.querySelectorAll("ul#channel-content > li")) {
+  for(const listItem of elems("ul#channel-content > li")) {
     if(listItem.getAttribute('data-timestamp') === snapshot.key)
       listItem.remove();
   }
 }
   
-async function chatChannelChildChanged(snapshot) {
-  for(const listItem of document.querySelectorAll("ul#channel-content > li")) {
-    if(listItem.getAttribute('data-timestamp') === snapshot.key) {
-      const newMessage = snapshot.val();
-      listItem.querySelector(".profile-pic").src = newMessage.user.photoURL;
-      listItem.querySelector(".username").innerHTML = `${newMessage.user.displayName} <time class="timestamp" datetime=${(new Date(parseInt(snapshot.key))).toISOString()}> ${(new Date(parseInt(snapshot.key))).toLocaleString('en-US', { dateStyle: "short", timeStyle: "short" })} </span>`;
-      listItem.querySelector(".message-content").textContent = newMessage.message;
-    }
+function chatChannelChildChanged(snapshot) {
+  if(messageElem = elems("ul#channel-content > li").find(v => v.getAttribute('data-timestamp') === snapshot.key)) {
+    const { message, user: { photoURL, displayName } } = snapshot.val();
+    messageElem.querySelector(".profile-pic").src = photoURL;
+    messageElem.querySelector(".username").firstChild.replaceWith(new Text(displayName + ' '));
+    messageElem.querySelector(".message-content").textContent = message;
   }
 }
 
@@ -261,15 +252,15 @@ async function loadMessages() {
     hashRef.child("messages").off("child_removed", chatChannelChildRemoved);
     hashRef.child("messages").off("child_changed", chatChannelChildChanged);
   }
-  elem("ul#channel-content").replaceChildren();
+  channelContent.replaceChildren();
   (elem(`a.channel[data-type]${location.hash}`) || createChannel(location.hash.slice(1), "chat", "chats")).closest("div.category[data-name]").querySelector("input[type='checkbox']").checked = true;
-  elem("ul#channel-content").setAttribute("data-channel", location.hash.slice(1));
+  channelContent.setAttribute("data-channel", location.hash.slice(1));
   hashRef = database.ref(`channels/${location.hash.slice(1)}`);
   hashRef.child("messages").on("child_added", chatChannelChildAdded);
   hashRef.child("messages").on("child_removed", chatChannelChildRemoved);
   hashRef.child("messages").on("child_changed", chatChannelChildChanged);
   elem("#message-input").value = '';
-  const readonly = (await hashRef.child("readonly").get()).val() || false;
+  const readonly = (await hashRef.child("readonly").get()).val();
   elem("#message-input").disabled = readonly;
   elem("#message-input").placeholder = readonly ? "You do not have permissions to send messages in this channel." : "Enter Message...";
 }
